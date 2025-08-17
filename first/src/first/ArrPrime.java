13e64f40be68e10f95cafda9ad96ef6a409d38ba
Aasma <aasmaakhtar9@gmail.com>
2025-08-17 14:12:33 +0530

    13 aug

---
+++ first/src/first/ArrPrime.java
@@ -0,0 +1,50 @@
+package first;
+
+import java.util.Scanner;
+
+public class ArrPrime {
+	public static void main(String[] args) {
+		int s,e;
+		System.out.println("Enter the start range");
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextInt();
+		System.out.println("Enter the end range");
+		e=sc.nextInt();
+		int [] a=new int[e-s];
+		int d=0;
+		int g=0;
+		for(int i=s;i<e;i++) {
+			int c=0;
+
+			for(int j=2;j*j<=i;j++) {
+				if(i%j==0) {
+					c=c+1;
+				}
+			}
+			if(c==0) {
+				g++;
+			}
+			
+		}
+		int [] p=new int[g];
+		for(int i=s;i<e;i++) {
+			int c=0;
+
+			for(int j=2;j*j<=i;j++) {
+				if(i%j==0) {
+					c=c+1;
+				}
+			}
+			if(c==0) {
+				a[d]=i;
+				d++;
+			}
+			
+		}
+		for(int i=0;i<d;i++) {
+			System.out.println(a[i]);
+		}
+		sc.close();
+
+	}
+}
